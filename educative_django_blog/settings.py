8fdd2e2bbdf277584489865cd253029929275929
ShafikurRahman <shafikshaon@gmail.com>
2021-12-04 22:53:37 +0600

    dockerize application

--- educative_django_blog/settings.py
+++ educative_django_blog/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ph$ihtf(1_r@_$%f**dt#v02ivgx_p#!-+u0_rfzznc590plf-'
+SECRET_KEY = 'ph$ihtf(1_r@_$%f**dt#v02ivgx_p#!-+u0_rfzznc590plf-'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.environ['ALLOWED_HOSTS']]
 
 
 # Application definition
@@ -48,6 +48,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+X_FRAME_OPTIONS = "allow-from https://educative.io"
 
 ROOT_URLCONF = 'educative_django_blog.urls'
 
